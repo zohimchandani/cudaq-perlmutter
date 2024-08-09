bc306c7fb06083ec90acd80c1cc13a3bd9f69336
Zohim Chandani <60148725+zohimchandani@users.noreply.github.com>
2024-08-09 10:33:09 +0100

    Update mgpu.sh

--- mgpu.sh
+++ mgpu.sh
@@ -12,4 +12,4 @@
 
 export CUDAQ_MPI_COMM_LIB=${HOME}/distributed_interfaces/libcudaq_distributed_interface_mpi.so
 
-srun shifter mpirun -np 8 python3 pmtest.py 
+srun shifter mpirun -np 8 python3 mgpu.py 
