f33462563885a8f6d8fcc5ebe270ecd6d43792d2
Zohim Chandani <60148725+zohimchandani@users.noreply.github.com>
2024-08-09 10:32:39 +0100

    Add files via upload

---
+++ mgpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH --gpus-per-task=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpu-bind=none
+#SBATCH -t 00:10:00
+#SBATCH -q debug
+#SBATCH -A m4642
+#SBATCH -C gpu
+#SBATCH --image=docker:nvcr.io/nvidia/nightly/cuda-quantum:latest
+#SBATCH --module=cuda-mpich
+
+export CUDAQ_MPI_COMM_LIB=${HOME}/distributed_interfaces/libcudaq_distributed_interface_mpi.so
+
+srun shifter mpirun -np 8 python3 pmtest.py 
