410278564a3cae47725224790c9f9571fbd542ee
Jan Balewski <janstar1122@gmail.com>
2024-08-22 10:03:49 -0700

    ok

--- ver1/big_sbatch.sh
+++ ver1/big_sbatch.sh
@@ -4,7 +4,9 @@ set -e  # bash exits if any statement returns a non-true return value
 
 comm=" -t 1:00:00 -q regular"
 #comm=''
-for N in 1 2 4 8 16 32 ; do
+
+#for N in 1 2 4 8 16 32 ; do
+for N in 64 128 256 ; do
     echo N=$N
     sbatch -N $N $comm batch_mgpu.slr 
 done
