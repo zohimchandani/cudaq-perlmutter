0ecce420ac564b7bb9bf1df705a3ab82d4230629
Zohim Chandani <zohim@login38.chn.perlmutter.nersc.gov>
2024-09-10 14:36:55 -0700

    fix

--- mpi/run_mpi_py.sh
+++ mpi/run_mpi_py.sh
@@ -10,6 +10,7 @@
 
 # This script allows us to allocate a single GPU 
 # to one of the MPI ranks.
+
 numGPUS=$(nvidia-smi --list-gpus | wc -l)
 echo $numGPUS
 CUDA_VISIBLE_DEVICES=$(($OMPI_COMM_WORLD_RANK % $numGPUS)) python3 $1
