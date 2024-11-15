624fd4a8087d1b46b6d3d6492a8b29b35802c8dc
Zohim Chandani <zohim@login30.chn.perlmutter.nersc.gov>
2024-11-15 15:43:02 -0800

    running with sc24 image

--- mqpu/hbatching.sh
+++ mqpu/hbatching.sh
@@ -7,11 +7,11 @@
 #SBATCH -q debug
 #SBATCH -A m4642
 #SBATCH -C gpu
-#SBATCH --image=docker:nvcr.io/nvidia/nightly/cuda-quantum:latest
+#SBATCH --image=nersc/sc_cuda_quantum:24.10
 #SBATCH --module=cuda-mpich
 
 export CUDAQ_MPI_COMM_LIB=${HOME}/distributed_interfaces/libcudaq_distributed_interface_mpi.so
 
-srun shifter python3 mqpu.py 
+srun shifter python3 hbatching.py 
 
 
